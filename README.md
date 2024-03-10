8bc86ba9a3b99ec23581b9aaaafe69fbb6499323
zackproject <zackprojectapp@gmail.com>
2024-03-10 04:34:23 +0100

    Add conection to mongodb with moongoose

--- README.md
+++ README.md
@@ -16,3 +16,12 @@ nodemon ./app.js
 # Running server
 ## Localhost
 localhost:3000
+
+# Connect with Backend
+[MongoDB](https://cloud.mongodb.com/)
+# Coleccions
+- cards (id:string, color:string, name:string)
+- users (id:string, username:string, password:string)
+
+Thanks to [
+Bluuweb](https://www.youtube.com/watch?v=VmH4tPbbDsM&list=PLPl81lqbj-4IEnmCXEJeEXPepr8gWtsl6&index=7)
--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
+const user = "";
+const password = "";
+const database = ""
+const uri = `mongodb+srv://${user}:${password}@cluster0.yleh4eg.mongodb.net/${database}?retryWrites=true&w=majority`;
+
+// to use mongoDB, installed mongoose
+const mongoose = require('mongoose');
+mongoose.connect(uri,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+).then(() => console.log('Connect to mongodb'))
+    .catch(e => console.log('Error connection', e))
 
 
 //dirname is the path of the server
